ac2a5689bbf9a12f3050b83dd04c6775987b9c78
Mitraws <m.ws@outlook.com>
2020-05-24 17:06:02 +0200

    create PostPage and dynamic route

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import React from 'react';
-import HomePage from './pages/HomePage';
-import './App.css';
+import React from "react";
+import HomePage from "./pages/HomePage";
+import PostPage from "./components/PostPage";
+import "./App.css";
 import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div>
       <Switch>
-        {/* more pages to be added here later */}
+        <Route path="/post/:id" component={PostPage} />
         <Route exact path="/" component={HomePage} />
       </Switch>
     </div>
